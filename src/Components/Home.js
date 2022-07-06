5fbb5607f22dbc6e1633cc7e26e77b8d39a57534
shivram yadav <shivramyadav52@gmail.com>
2022-07-06 20:41:57 +0530

    first commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Tabs defaultIndex={0} onSelect={(index) => console.log(index)}>
+      <TabList>
+        <Tab>Title 1</Tab>
+        <Tab>Title 2</Tab>
+        <Tab>Title 3</Tab>
+      </TabList>
+
+      <TabPanel>
+        <h2>Any content 1</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>Any content 2</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>Any content 3</h2>
+      </TabPanel>
+    </Tabs>
+  );
 };
 
 export default Home;
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "../reducers/index";
@@ -8,8 +8,12 @@ import rootSaga from "../sagas";
 const sagaMiddleware = createSagaMiddleware();
 // we can pass multiple middleware ex: logger
 const middleware = [sagaMiddleware];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // mount it on the Store
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
 // then run the saga
 sagaMiddleware.run(rootSaga);
